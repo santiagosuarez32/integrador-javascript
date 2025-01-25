d41316ce29dd007de6cc4223a6848de9e50cfbc1
santiagosuarez32 <dlsantii86@gmail.com>
2025-01-25 19:21:34 -0300

    sdf

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  build: {
-    outDir: "dist",
-  },
-
-  server: {
-    open: true,
-  },
-
   plugins: [tailwindcss()],
 });
