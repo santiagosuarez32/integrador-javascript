a78d16df499cf73e2a3b0a34d6f996ae8b4c5639
santiagosuarez32 <dlsantii86@gmail.com>
2025-01-29 12:34:30 -0300

    Sdf

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    extend: {
+      animation: {
+        "gradient-border": "gradientBorderAnimation 8s ease infinite",
+      },
+      keyframes: {
+        gradientBorderAnimation: {
+          "0%": { "background-position": "0% 50%" },
+          "50%": { "background-position": "100% 50%" },
+          "100%": { "background-position": "0% 50%" },
+        },
+      },
+    },
+  },
+};
