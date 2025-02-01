778ee3c86a1e4a9afb8e54f592e2ec94bdbee849
santiagosuarez32 <dlsantii86@gmail.com>
2025-02-01 17:28:54 -0300

    DS

--- assets/data.js
+++ assets/data.js
@@ -3,56 +3,121 @@ const productsData = [
     id: 1,
     name: "Ryzen 5600",
     precio: 12000,
-    category: "hardware",
-    cardImg: "./img/ryzen-5600.jpg",
+    descripcion: "Componente hardware gaming",
+    // category: "hardware",
+    cardImg: "assets/img/products/disco-hdd.webp",
   },
   {
-    id: 1,
-    name: "Ryzen 5600",
+    id: 2,
+    name: "Ryzen 5700",
     precio: 12000,
-    category: "hardware",
-    cardImg: "./img/ryzen-5600.jpg",
+    descripcion: "Componente hardware gaming",
+    // category: "hardware",
+    cardImg: "assets/img/products/cooler.jpg",
   },
   {
-    id: 1,
+    id: 3,
     name: "Ryzen 5600",
-    precio: 12000,
-    category: "hardware",
-    cardImg: "./img/ryzen-5600.jpg",
+    precio: 1200220,
+    descripcion: "Componente hardware gaming",
+    // category: "hardware",
+    cardImg: "assets/img/products/memory.jpg",
   },
   {
-    id: 1,
+    id: 4,
     name: "Ryzen 5600",
     precio: 12000,
-    category: "hardware",
-    cardImg: "./img/ryzen-5600.jpg",
+    descripcion: "Componente hardware gaming",
+    // category: "hardware",
+    cardImg: "assets/img/ryzen-5600.jpg",
   },
   {
-    id: 1,
+    id: 5,
     name: "Ryzen 5600",
     precio: 12000,
-    category: "hardware",
-    cardImg: "./img/ryzen-5600.jpg",
+    descripcion: "Componente hardware gaming",
+    // category: "hardware",
+    cardImg: "assets/img/ryzen-5600.jpg",
   },
   {
-    id: 1,
+    id: 6,
     name: "Ryzen 5600",
     precio: 12000,
-    category: "hardware",
-    cardImg: "./img/ryzen-5600.jpg",
+    descripcion: "Componente hardware gaming",
+    // category: "hardware",
+    cardImg: "assets/img/ryzen-5600.jpg",
   },
   {
-    id: 1,
+    id: 7,
     name: "Ryzen 5600",
     precio: 12000,
-    category: "hardware",
-    cardImg: "./img/ryzen-5600.jpg",
+    descripcion: "Componente hardware gaming",
+    // category: "hardware",
+    cardImg: "assets/img/ryzen-5600.jpg",
   },
   {
-    id: 1,
+    id: 8,
     name: "Ryzen 5600",
     precio: 12000,
-    category: "hardware",
-    cardImg: "./img/ryzen-5600.jpg",
+    descripcion: "Componente hardware gaming",
+    // category: "hardware",
+    cardImg: "assets/img/ryzen-5600.jpg",
+  },
+  {
+    id: 9,
+    name: "Ryzen 5600",
+    precio: 12000,
+    descripcion: "Componente hardware gaming",
+    // category: "hardware",
+    cardImg: "assets/img/ryzen-5600.jpg",
+  },
+  {
+    id: 10,
+    name: "Ryzen 5600",
+    precio: 12000,
+    descripcion: "Componente hardware gaming",
+    // category: "hardware",
+    cardImg: "assets/img/ryzen-5600.jpg",
+  },
+  {
+    id: 11,
+    name: "Ryzen 5600",
+    precio: 12000,
+    descripcion: "Componente hardware gaming",
+    // category: "hardware",
+    cardImg: "assets/img/ryzen-5600.jpg",
+  },
+  {
+    id: 12,
+    name: "Ryzen 5600",
+    precio: 12000,
+    descripcion: "Componente hardware gaming",
+    // category: "hardware",
+    cardImg: "assets/img/ryzen-5600.jpg",
   },
 ];
+
+const divideProductsInParts = (size) => {
+  let productList = [];
+
+  for (let i = 0; i < productsData.length; i += size) {
+    productList.push(productsData.slice(i, i + size));
+  }
+  return productList;
+};
+
+// console.log(divideProductsInParts(6));
+
+// console.log(productsData.slice(0, 6));
+
+const PRODUCTS_SIZE = 6;
+
+const appState = {
+  products: divideProductsInParts(PRODUCTS_SIZE),
+  productsLimit: divideProductsInParts(PRODUCTS_SIZE).length,
+  currentProductsIndex: 0,
+};
+
+console.log(appState);
+
+// console.log(appState.productsLimit);
--- assets/index.js
+++ assets/index.js
@@ -1,43 +1,75 @@
-window.onbeforeunload = function () {
-  window.scrollTo(0, 0);
-};
+// window.onbeforeunload = function () {
+//   window.scrollTo(0, 0);
+// };
 
-document.addEventListener("DOMContentLoaded", function () {
-  const loadMoreButton = document.getElementById("load-more-button");
-  const cards = document.querySelectorAll("#hidden-card");
-  let currentIndex = 0;
+// document.addEventListener("DOMContentLoaded", function () {
+//   const loadMoreButton = document.getElementById("load-more-button");
+//   const cards = document.querySelectorAll("#hidden-card");
+//   let currentIndex = 0;
 
-  loadMoreButton.addEventListener("click", function () {
-    for (let i = 0; i < 4 && currentIndex < cards.length; i++) {
-      cards[currentIndex].classList.remove("hidden");
-      currentIndex++;
-    }
-    if (currentIndex >= cards.length) {
-      loadMoreButton.style.display = "none";
-    }
-  });
-});
+//   loadMoreButton.addEventListener("click", function () {
+//     for (let i = 0; i < 4 && currentIndex < cards.length; i++) {
+//       cards[currentIndex].classList.remove("hidden");
+//       currentIndex++;
+//     }
+//     if (currentIndex >= cards.length) {
+//       loadMoreButton.style.display = "none";
+//     }
+//   });
+// });
 
 // const cartButton = document
 //   .getElementById("cart-button")
 //   .addEventListener("click", function() );
 
-document.getElementById("cart-button").addEventListener("click", function () {
-  document.getElementById("cart-menu").classList.remove("hidden");
-});
+// document.getElementById("cart-button").addEventListener("click", function () {
+//   document.getElementById("cart-menu").classList.remove("hidden");
+// });
 
-document.getElementById("button-close").addEventListener("click", function () {
-  document.getElementById("cart-menu").classList.add("hidden");
-});
-document.getElementById("cart-button2").addEventListener("click", function () {
-  document.getElementById("cart-menu").classList.remove("hidden");
-});
+// document.getElementById("button-close").addEventListener("click", function () {
+//   document.getElementById("cart-menu").classList.add("hidden");
+// });
+// document.getElementById("cart-button2").addEventListener("click", function () {
+//   document.getElementById("cart-menu").classList.remove("hidden");
+// });
 
-document.getElementById("button-close").addEventListener("click", function () {
-  document.getElementById("cart-menu").classList.add("hidden");
-});
+// document.getElementById("button-close").addEventListener("click", function () {
+//   document.getElementById("cart-menu").classList.add("hidden");
+// });
 
-document.getElementById("hamb-menu").addEventListener("click", function () {
-  const menu = document.getElementById("menu");
-  menu.classList.toggle("hidden");
-});
+// document.getElementById("hamb-menu").addEventListener("click", function () {
+//   const menu = document.getElementById("menu");
+//   menu.classList.toggle("hidden");
+// });
+
+const productsContainer = document.querySelector("#products-container");
+
+const createProductTemplate = (product) => {
+  const { id, name, precio, descripcion, cardImg } = product;
+  return `<div class="bg-white rounded-xl p-4 cursor-pointer hover:-translate-y-1 transition-all relative">    
+          <div class="mb-4 bg-gray-100 rounded p-2">
+          <img src="${cardImg}" alt="${name}" class="aspect-[33/35] w-full object-contain" />
+          </div>
+          <div>
+            <div class="flex gap-2">
+              <h5 class="text-base font-bold text-gray-800">${name}</h5>
+              <h6 class="text-base text-gray-800 font-bold ml-auto">${precio}</h6>
+            </div>
+            <p class="text-gray-500 text-[13px] mt-2">${descripcion}</p>
+            <div class="flex items-center gap-2 mt-4">
+             
+            <button type="button" class="text-sm px-2 h-9 font-semibold w-full bg-red-600 hover:bg-red-700 text-white tracking-wide ml-auto outline-none border-none rounded">Agregar</button>
+          </div>
+        </div>
+      </div>`;
+};
+
+const renderProducts = (products) => {
+  productsContainer.innerHTML = products.map(createProductTemplate).join("");
+};
+
+const init = () => {
+  renderProducts(productsData);
+};
+
+init();
