3158b389e61f3236e150c9f435574ca04a715535
santiagosuarez32 <dlsantii86@gmail.com>
2025-02-07 17:17:28 -0300

    SDfs

--- assets/data.js
+++ assets/data.js
@@ -2,7 +2,7 @@ const productsData = [
   {
     id: 1,
     name: "Auriculares",
-    precio: 12000,
+    precio: 800,
     descripcion: "Auriculares para tener la mejor experiencia gamer",
     category: "perifericos",
     cardImg: "assets/img/products/auriculares.webp",
@@ -10,7 +10,7 @@ const productsData = [
   {
     id: 2,
     name: "Cooler CPU",
-    precio: 12000,
+    precio: 760,
     descripcion: "Ideal para la refrigeración y mantenimiento del cpu",
     category: "hardware",
     cardImg: "assets/img/products/cooler.jpg",
@@ -18,7 +18,7 @@ const productsData = [
   {
     id: 3,
     name: "Pad Gamer",
-    precio: 12000,
+    precio: 330,
     descripcion: "Ideal para tener la mayor comodidad en tu set-up",
     category: "perifericos",
     cardImg: "assets/img/products/pad.jpg",
@@ -26,7 +26,7 @@ const productsData = [
   {
     id: 4,
     name: "Disco HDD",
-    precio: 1200220,
+    precio: 560,
     descripcion: "Adquiere mayor velocidad de lectura en tu Pc",
     category: "hardware",
     cardImg: "assets/img/products/disco-hdd.webp",
@@ -34,7 +34,7 @@ const productsData = [
   {
     id: 5,
     name: "Disco SSD",
-    precio: 12000,
+    precio: 670,
     descripcion: "La mejor opción si deseas tener velocidad en tu Pc",
     category: "hardware",
     cardImg: "assets/img/products/disco-ssd.jpg",
@@ -42,7 +42,7 @@ const productsData = [
   {
     id: 6,
     name: "Fuente",
-    precio: 12000,
+    precio: 780,
     descripcion: "Alimenta y optimiza tus componentes con esta fuente",
     category: "hardware",
     cardImg: "assets/img/products/fuente-de-poder.webp",
@@ -50,7 +50,7 @@ const productsData = [
   {
     id: 7,
     name: "Intel I13",
-    precio: 12000,
+    precio: 653,
     descripcion: "Obtiene la mayor velocidad en tu Pc con esta opción",
     category: "hardware",
     cardImg: "assets/img/products/intel.webp",
@@ -58,7 +58,7 @@ const productsData = [
   {
     id: 8,
     name: "Gabinete",
-    precio: 12000,
+    precio: 700,
     descripcion: "Ensambla y organiza tus componentes Gaming",
     category: "perifericos",
     cardImg: "assets/img/products/gabinete.jpg",
@@ -66,7 +66,7 @@ const productsData = [
   {
     id: 9,
     name: "Tarjeta de video",
-    precio: 12000,
+    precio: 800,
     descripcion: "Disfruta de los mejores juegos con esta opción",
     category: "hardware",
     cardImg: "assets/img/products/tarjeta-de-video.jpg",
@@ -74,7 +74,7 @@ const productsData = [
   {
     id: 10,
     name: "Memoria Ram",
-    precio: 12000,
+    precio: 670,
     descripcion: "Obtiene el mejor desempeño en tus juegos",
     category: "hardware",
     cardImg: "assets/img/products/memory.jpg",
@@ -82,7 +82,7 @@ const productsData = [
   {
     id: 11,
     name: "Monitor",
-    precio: 12000,
+    precio: 700,
     descripcion: "Ideal para tener una mejor experiencia Gaming",
     category: "perifericos",
     cardImg: "assets/img/products/monitor.webp",
@@ -90,7 +90,7 @@ const productsData = [
   {
     id: 12,
     name: "Mouse",
-    precio: 12000,
+    precio: 820,
     descripcion: "Mejora tu jugabilidad y competitividad",
     category: "perifericos",
     cardImg: "assets/img/products/mouse.jpeg",
@@ -98,7 +98,7 @@ const productsData = [
   {
     id: 13,
     name: "Placa madre",
-    precio: 12000,
+    precio: 125,
     descripcion: "Mayor compatibilidad con nuevos componentes",
     category: "hardware",
     cardImg: "assets/img/products/placa-madre.jpg",
@@ -106,7 +106,7 @@ const productsData = [
   {
     id: 14,
     name: "Cámara PC",
-    precio: 1200,
+    precio: 350,
     descripcion: "Ideal para refrigerar tus componentes",
     category: "perifericos",
     cardImg: "assets/img/products/camara.jpg",
@@ -114,7 +114,7 @@ const productsData = [
   {
     id: 15,
     name: "Microfono PC",
-    precio: 12000,
+    precio: 170,
     descripcion: "Tarjeta de video potente y ideal para gaming",
     category: "perifericos",
     cardImg: "assets/img/products/microfono.jpg",
@@ -123,7 +123,7 @@ const productsData = [
   {
     id: 16,
     name: "Teclado Gamer",
-    precio: 12000,
+    precio: 220,
     descripcion: "Mejor jugabilidad y diseño en tus juegos",
     category: "perifericos",
     cardImg: "assets/img/products/teclado-mecanico.webp",
@@ -148,4 +148,4 @@ const appState = {
   activeFilter: null,
 };
 
-console.log({ appState });
+// console.log({ appState });
--- assets/index.js
+++ assets/index.js
@@ -12,10 +12,16 @@ const overlay = document.querySelector(".overlay");
 const menuBtn = document.querySelector(".cart-button2");
 const hambMenu = document.querySelector(".open-hamb");
 const menu = document.querySelector(".hamb-menu");
+const total = document.querySelector(".total");
+const successModal = document.querySelector(".add-modal");
+
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+const saveCart = () => localStorage.setItem("cart", JSON.stringify(cart));
 
 const createProductTemplate = (product) => {
   const { id, name, precio, category, descripcion, cardImg } = product;
-  return `<div class="bg-white rounded-xl p-4 cursor-pointer hover:-translate-y-1 transition-all relative product">    
+  return `<div class="bg-white rounded-xl p-4 cursor-pointer hover:-translate-y-1 transition-all relative">    
           <div class="mb-4 bg-gray-100 rounded p-2">
           <img src="${cardImg}" alt="${name}" class="aspect-[33/35] w-full object-contain" />
           </div>
@@ -27,7 +33,7 @@ const createProductTemplate = (product) => {
             <p class="text-gray-500 text-[13px] mt-2">${descripcion}</p>
             <div class="flex items-center gap-2 mt-4">
              
-            <button type="button" class="text-sm px-2 h-9 font-semibold w-full bg-red-600 hover:bg-red-700 text-white tracking-wide ml-auto outline-none border-none rounded">Agregar</button>
+            <button type="button" class="text-sm px-2 h-9 font-semibold w-full bg-red-600 hover:bg-red-700 text-white tracking-wide ml-auto outline-none border-none rounded btn-add" data-id="${id}" data-name="${name}" data-price="${precio}" data-img="${cardImg}" data-desc="${descripcion}" >Agregar</button>
           </div>
         </div>
       </div>`;
@@ -76,6 +82,16 @@ const changeBtnActiveState = (activeFilter) => {
   });
 };
 
+const showCartTotal = () => {
+  total.innerHTML = `$${getCartTotal()}`;
+};
+
+const updateCartState = () => {
+  saveCart();
+  showCartTotal();
+  renderCart();
+};
+
 const isInactiveFilterBtn = (element) => {
   return (
     element.classList.contains("category") &&
@@ -105,73 +121,136 @@ const addCartMobile = () => {
   cartMenu.classList.remove("hidden");
 };
 
-// const closeCartMobile = () => {
-//   cartMenu.classList.add("hidden");
-// };
-
 const openMenuHamb = () => {
   menu.classList.toggle("hidden");
+};
+
+const isExistingCartProduct = (product) => {
+  return cart.find((item) => item.id === product.id);
+};
+
+const renderCart = () => {
+  if (!cart.length) {
+    productsCart.innerHTML = `<p> No hay productos en el carrito</p>`;
+    return;
+  }
+  productsCart.innerHTML = cart.map(createCartProductHTML).join("");
+};
+
+const getCartTotal = () => {
+  const total = cart.reduce(
+    (acc, cur) => acc + Number(cur.price) * cur.quantity,
+    0
+  );
+  console.log(total);
+  return total;
+};
+
+const createCartProductHTML = (cartProduct) => {
+  const { id, name, price, img, quantity, desc } = cartProduct;
+  return `<div class="flex justify-center">
+  <div class="product-card bg-white rounded-lg shadow-md p-4 mb-4 transition transform hover:scale-105 max-w-lg flex justify-between">
+    <div class="flex items-center">
+      <img src="${img}" alt="Pad Gamer" class="w-16 h-16 rounded-md mr-4 border border-gray-200">
+      <div class="flex flex-col">
+        <h3 class="text-md font-bold text-gray-900 mb-2">${name}</h3>
+        <p class="text-sm text-gray-600 mb-2">${desc}</p>
+        <p class="text-md font-extrabold text-black mt-1">$${price}</p>
+      </div>
+    </div>
+    <div class="flex items-center">
+      <button class="quantity-down bg-gray-300 text-gray-800 py-1 px-3 rounded-lg hover:bg-gray-400" data-id="${id}">-</button>
+      <span class="mx-2 text-lg font-semibold text-gray-800">${quantity}</span>
+      <button class="quantity-up bg-gray-300 text-gray-800 py-1 px-3 rounded-lg hover:bg-gray-400" data-id="${id}" >+</button>
+    </div>
+  </div>
+</div>
+
+`;
+};
+
+const addProduct = (e) => {
+  if (!e.target.classList.contains("btn-add")) return;
+  const product = createProductData(e.target.dataset);
+
+  if (isExistingCartProduct(product)) {
+    addUnitToProduct(product);
+  } else {
+    cart = [...cart, { ...product, quantity: 1 }];
+  }
+  updateCartState();
+  console.log(cart);
+};
+
+const addUnitToProduct = (product) => {
+  // console.log("existe en el carro, le agrego uno mas");
+  cart = cart.map((cartProduct) =>
+    cartProduct.id === product.id
+      ? { ...cartProduct, quantity: cartProduct.quantity + 1 }
+      : cartProduct
+  );
+};
+
+const createProductData = (product) => {
+  const { id, name, price, img, desc } = product;
+  return { id, name, price, img, desc };
+};
+
+const handlePlusEvent = (id) => {
+  const existingCartProduct = cart.find((item) => item.id === id);
+  console.log(existingCartProduct);
+  addUnitToProduct(existingCartProduct);
+};
+
+const handleMinusBtnEvent = (id) => {
+  const existingCartProduct = cart.find((item) => item.id === id);
+
+  if (existingCartProduct.quantity === 1) {
+    removeProductFromCart(existingCartProduct);
+    return;
+  }
+
+  substractProductUnit(existingCartProduct);
+};
+
+const removeProductFromCart = (existingProduct) => {
+  cart = cart.filter((product) => product.id !== existingProduct.id);
+  updateCartState();
+};
+
+const substractProductUnit = (existingProduct) => {
+  cart = cart.map((product) => {
+    return product.id === existingProduct.id
+      ? { ...product, quantity: Number(product.quantity) - 1 }
+      : product;
+  });
+};
+
+const handleQuantity = (e) => {
+  // console.log(e.target);
+  if (e.target.classList.contains("quantity-down")) {
+    console.log("resta");
+    handleMinusBtnEvent(e.target.dataset.id);
+  } else if (e.target.classList.contains("quantity-up")) {
+    // console.log("suma");
+    handlePlusEvent(e.target.dataset.id);
+  }
+
+  updateCartState();
 };
 
 const init = () => {
   renderProducts(appState.products[0]);
   showMoreBtn.addEventListener("click", showMoreProducts);
   categoriesContainer.addEventListener("click", applyFilter);
-
   cartBtn.addEventListener("click", addCart);
   closeBtn.addEventListener("click", closeCart);
   menuBtn.addEventListener("click", addCartMobile);
-  // closeBtn.addEventListener("click", closeCartMobile);
   hambMenu.addEventListener("click", openMenuHamb);
+  productsContainer.addEventListener("click", addProduct);
+  document.addEventListener("DOMContentLoaded", renderCart);
+  document.addEventListener("DOMContentLoaded", showCartTotal);
+  productsCart.addEventListener("click", handleQuantity);
 };
 
 init();
-
-/* <ul role="list" class="-my-6 divide-y divide-gray-200">
-                    <!-- Aquí van los elementos del carrito -->
-                    <ul role="list" class="-my-6 divide-y divide-gray-200">
-                      <li class="flex py-6">
-                        <div class="size-24 shrink-0 overflow-hidden rounded-md border border-gray-200">
-                          <img src="" alt="" class="size-full object-cover">
-                        </div>
-                        <div class="ml-4 flex flex-1 flex-col">
-                          <div>
-                            <div class="flex justify-between text-base font-medium text-white">
-                              <h3>
-                                <a href="#"></a>
-                              </h3>
-                              <p class="ml-4"></p>
-                            </div>
-                            <p class="mt-1 text-sm text-white"></p>
-                          </div>
-                          <div class="flex flex-1 items-end justify-between text-sm">
-                            <p class="text-white"></p>
-                            <div class="flex">
-                              <button type="button" class="font-medium text-red-600 hover:text-red-500">Remove</button>
-                            </div>
-                          </div>
-                        </div>
-                      </li>
-                      <li class="flex py-6">
-                        <div class="size-24 shrink-0 overflow-hidden rounded-md border border-gray-200">
-                          <img src="" alt="" class="size-full object-cover">
-                        </div>
-                        <div class="ml-4 flex flex-1 flex-col">
-                          <div>
-                            <div class="flex justify-between text-base font-medium text-white">
-                              <h3>
-                                <a href="#"></a>
-                              </h3>
-                              <p class="ml-4"></p>
-                            </div>
-                            <p class="mt-1 text-sm text-white"></p>
-                          </div>
-                          <div class="flex flex-1 items-end justify-between text-sm">
-                            <p class="text-white"></p>
-                            <div class="flex">
-                              <button type="button" class="font-medium text-red-600 hover:text-red-500">Remove</button>
-                            </div>
-                          </div>
-                        </div>
-                      </li>
-                    </ul> */
